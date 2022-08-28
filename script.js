0f2d0dd1221fad476b35d6eff3a381168d1bcbd8
Mcyates <mcyatesvm@gmail.com>
2022-08-28 00:02:45 -0500

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+//function userName(){
+//    let unjs = document.getElementById('unjs');
+//    let unj = document.getElementById('unj');
+//}
+
+//userName();
+//document.getElementById('unjs').innerText = unjs;
+//document.getElementById('unj').innerText = unjs;
